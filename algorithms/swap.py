4e28bc940fe760bf0cffb25b3cbe7ff91ee017ff
Greg Reynolds <gregoryareynoldsjr@gmail.com>
2022-03-31 16:40:11 -0700

    put swap under algorithms

---
+++ algorithms/swap.py
@@ -0,0 +1,13 @@
+def swap(first, second):
+    print ("swap ", first, second)
+    temp = first
+    first = second
+    second = temp
+    return first, second
+
+def swap_items():
+        # get user choice
+    first = input("Enter first number: ")
+    second = input("Enter second number: ")
+    first, second = swap(first, second)
+    print ("Swap result: ", first, second)
