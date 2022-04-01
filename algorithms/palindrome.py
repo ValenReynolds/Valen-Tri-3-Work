395b369fc576cae40ad395a2e3027831065faaa3
Greg Reynolds <gregoryareynoldsjr@gmail.com>
2022-03-31 17:57:40 -0700

    better
    Signed-off-by: Greg Reynolds <gregoryareynoldsjr@gmail.com>

--- algorithms/palindrome.py
+++ algorithms/palindrome.py
@@ -22,7 +22,7 @@ class Palindrome:
         self._length = len(candidate)  # input length
         # analysis values
         self._is_a_palindrome = False  # initialize status
-        self._az09 = re.sub(r'[^a-zA-Z0-9]', '', self._candidate)  # alpha numeric characters
+        self._az09 = re.sub(r'[^a-zA-Z0-9]', '', self._candidate) # non alpha numeric characters
         self._analysis = []  # array of tests
         self._tests = 0  # counter of tests performed
         # evaluate for palindrome
@@ -30,8 +30,11 @@ class Palindrome:
 
     # palindrome tester method
     def is_palindrome(self):
+        # validate palindrome candidate only contains alphanumeric characters
         c = self._az09
-        if re.search("^[a-zA-Z0-9]", self._candidate) == None:
+        compiled = re.compile("[a-zA-Z0-9]")
+        result = compiled.findall(self._candidate)
+        if (len(result) < len(self._candidate)):
             self._is_a_palindrome = False
             return False
         # Run loop from 0 to len/2 of string (middle is exit point)
@@ -80,6 +83,8 @@ if __name__ == '__main__':
     assert(Palindrome("abcdeffedcba").isPalindrome == True)  
     # Sadness is a fail
     assert(Palindrome("Sadness").isPalindrome == False)
+    # ()&*) is a fail
+    assert(Palindrome("()&*)").isPalindrome == False)
     # longest true Palindrome
     if (Palindrome("tattarrattat").isPalindrome):
         print ("tattarrattat is a Palindrome")
